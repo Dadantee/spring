30537ee02e679faab4829c655be5c8c3e0c666b5
Alex <ArcizCDMA@gmail.com>
2024-10-21 11:22:25 +0300

    Test book "ok"

--- src/main/java/com/inversion/spring/book/controller/BookApiController.java
+++ src/main/java/com/inversion/spring/book/controller/BookApiController.java
@@ -1,2 +1,15 @@
-package com.inversion.spring.book.controller;public class BookApiController {
+package com.inversion.spring.book.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookApiController {
+
+
+    @GetMapping("/")
+    public String ok(){
+        return "ok";
+
+    }
 }
